e3a7dae0a0da1e56786e27ddc7c2f0bd245e610c
Mrutyunjaya Debata <mdmrutyunjayadebata@gmail.com>
2026-06-04 12:50:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,13 @@ import tensorflow as tf
 import numpy as np
 from PIL import Image
 
+# MUST be the first Streamlit command
+st.set_page_config(
+    page_title="Deepfake Detection System",
+    page_icon="🛡️",
+    layout="centered"
+)
+
 # Load model once
 @st.cache_resource
 def load_model():
@@ -11,12 +18,6 @@ def load_model():
 model = load_model()
 
 IMG_SIZE = 224
-
-st.set_page_config(
-    page_title="Deepfake Detection System",
-    page_icon="🛡️",
-    layout="centered"
-)
 
 st.title("🛡️ Deepfake Detection System")
 st.write("Upload an image to check whether it is REAL or FAKE.")
@@ -28,32 +29,39 @@ uploaded_file = st.file_uploader(
 
 if uploaded_file is not None:
 
-    # Open image and convert to RGB
     image = Image.open(uploaded_file).convert("RGB")
 
-    # Display image
-    st.image(image, caption="Uploaded Image", width=300)
+    st.image(
+        image,
+        caption="Uploaded Image",
+        width=300
+    )
 
-    # Resize image
     image = image.resize((IMG_SIZE, IMG_SIZE))
 
-    # Convert to numpy array
     img = np.array(image)
 
-    # Normalize
     img = img / 255.0
 
-    # Add batch dimension
     img = np.expand_dims(img, axis=0)
 
-    # Predict
-    prediction = model.predict(img, verbose=0)[0][0]
+    prediction = model.predict(
+        img,
+        verbose=0
+    )[0][0]
 
     st.subheader("Prediction Result")
 
     if prediction > 0.65:
-        st.error(f"🚨 FAKE (Confidence: {prediction:.2f})")
+        st.error(
+            f"🚨 FAKE (Confidence: {prediction:.2f})"
+        )
     else:
-        st.success(f"✅ REAL (Confidence: {1 - prediction:.2f})")
+        st.success(
+            f"✅ REAL (Confidence: {1-prediction:.2f})"
+        )
 
-    st.write("Raw Confidence Score:", round(float(prediction), 4))
+    st.write(
+        "Raw Confidence Score:",
+        round(float(prediction), 4)
+    )
